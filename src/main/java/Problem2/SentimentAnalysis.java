6892254de0370971b08e2d52355c7ccb347607ee
Maan Mandaliya <mn638205@dal.ca>
2021-11-26 21:43:38 -0400

    Project structure created

---
+++ src/main/java/Problem2/SentimentAnalysis.java
@@ -0,0 +1,39 @@
+package Problem2;
+
+import java.util.Iterator;
+
+import org.bson.Document;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+
+public class SentimentAnalysis {
+	public static void main(String args[]) {
+		// MongoDB Configurations
+		ConnectionString connectionString = new ConnectionString(
+				"mongodb+srv://root:root@cluster0.jvs2l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+		MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(connectionString).build();
+		MongoClient mongoClient = MongoClients.create(settings);
+		MongoDatabase cleaned_database = mongoClient.getDatabase("ProcessedDb");
+		
+		//All collections
+		MongoIterable<String> collections = cleaned_database.listCollectionNames();
+		for (String collection : collections) {
+			MongoCollection current_collection = cleaned_database.getCollection(collection);
+			FindIterable<Document> documents = current_collection.find();
+			Iterator iterator = documents.iterator();
+			while(iterator.hasNext())
+			{
+				Document document = iterator.next();
+				
+			}
+		}
+
+	}
+}
