fe542c1f53cd1e14eef9d36e8438fbd611c4cf7d
Maan Mandaliya <mn638205@dal.ca>
2021-11-26 23:21:11 -0400

    Sentiment Analysis updated again

--- src/main/java/Problem2/SentimentAnalysis.java
+++ src/main/java/Problem2/SentimentAnalysis.java
@@ -1,17 +1,15 @@
 package Problem2;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Formatter;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -40,6 +38,10 @@ public class SentimentAnalysis {
 		// All collections
 		MongoIterable<String> collections = cleaned_database.listCollectionNames();
 
+		// Formatting the table
+		Formatter format = new Formatter();
+		format.format("%106s\n", "-".repeat(60));
+		format.format("|%15s|%15s|%50s|%10s|%10s|\n","News Article","News Content","Matched Words","Overall Score","Polarity");
 		// Collection wise operation
 		for (String collection : collections) {
 			// Temporary collection
@@ -47,6 +49,7 @@ public class SentimentAnalysis {
 			FindIterable<Document> documents = current_collection.find();
 			MongoCursor<Document> cursor = documents.iterator();
 
+			int article_count = 1;
 			// Iterating over all documents inside a single collection
 			while (cursor.hasNext()) {
 				Document document = cursor.next();
@@ -57,33 +60,64 @@ public class SentimentAnalysis {
 				// To count bag of words
 				Map<String, Integer> bag_of_words = new HashMap<String, Integer>();
 				String[] words = tweet.split(" ");
-				//For each loop for all words in bag of words
+				// For each loop for all words in bag of words
 				for (String word : words) {
-					if(bag_of_words.containsKey(word))
-					{
-						bag_of_words.put(word,bag_of_words.get(word)+1);
-					}
-					else
-					{
-						bag_of_words.put(word,1);
+					if (bag_of_words.containsKey(word)) {
+						bag_of_words.put(word, bag_of_words.get(word) + 1);
+					} else {
+						bag_of_words.put(word, 1);
 					}
 				}
-				
-				//Get all words from negative words file
+
+				// Get all words from negative words file
 				URL negative_words_url = SentimentAnalysis.class.getClassLoader().getResource("negative-words.txt");
 //				BufferedReader br = new BufferedReader(new FileReader(new File(negative_words_url.toURI())));
 //				Path path = Paths.get(negative_words_url.toURI().getPath());
 				File negative_file = new File(negative_words_url.toURI());
 				Path negative_path = Paths.get(negative_file.getAbsolutePath());
 				List<String> negative_words = Files.readAllLines(negative_path);
-				
-				//Get all words from positive words file
+
+				// Get all words from positive words file
 				URL positive_words_url = SentimentAnalysis.class.getClassLoader().getResource("positive-words.txt");
 				File positive_file = new File(negative_words_url.toURI());
 				Path positive_path = Paths.get(positive_file.getAbsolutePath());
 				List<String> positive_words = Files.readAllLines(positive_path);
+
+				// List for words that has been matched with either of words
+				ArrayList<String> matched_words = new ArrayList<String>();
+				// To calculate polarity of tweet
+				int overall_score = 0;
+
+				// Calculate polarity of tweet
+				for (String word : words) {
+					if (negative_words.contains(word)) {
+						overall_score--;
+						matched_words.add(word);
+					} else if (positive_words.contains(word)) {
+						overall_score++;
+						matched_words.add(word);
+					}
+				}
+				
+				//Setting up polarity of news article
+				String polarity;
+				if(overall_score<0) {
+					polarity="Negative"; 
+				}
+				else if(overall_score>0)
+				{
+					polarity="Positive";
+				}
+				else
+				{
+					polarity="Neutral";
+				}
+				
+				format.format("|%15s|%15s|%50s|%10s|%10s|\n",article_count,tweet,matched_words.toString(),overall_score,polarity);
+				article_count++;
 			}
 		}
+		System.out.println(format);
 
 	}
 }
